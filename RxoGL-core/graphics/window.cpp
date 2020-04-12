00b4290e3cb39f5b78b4179baf90a918ff61d980
dylanhan99 <dylanhan99@gmail.com>
2020-04-12 22:16:01 +0800

    Implemented input for keyboard, mouse and mouse position
    To move the code into its own input class in the future

---
+++ RxoGL-core/graphics/window.cpp
@@ -0,0 +1,127 @@
+#include "window.h"
+
+namespace rxogl { namespace graphics {
+
+	void window_resize(GLFWwindow* window, int width, int height);
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+	void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+
+	Window::Window(const char* title, int width, int height)
+	{
+		m_Title = title;
+		m_Width = width;
+		m_Height = height;
+
+		if (!init())
+			glfwTerminate();
+
+		for (int i = 0; i < MAX_KEYS; i++)
+		{
+			m_Keys[i] = false;
+		}
+		for (int i = 0; i < MAX_BUTTONS; i++)
+		{
+			m_MouseButtons[i] = false;
+		}
+	}
+
+	Window::~Window()
+	{
+		glfwTerminate();
+	}
+
+	bool Window::init()
+	{
+		if (!glfwInit())
+		{
+			std::cout << "Failed to create GLFW!" << std::endl;
+			return false;
+		}
+
+		m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+		if (!m_Window)
+		{
+			std::cout << "Failed to create GLFW window!" << std::endl;
+			return false;
+		}
+		
+		glfwMakeContextCurrent(m_Window);
+		glfwSetWindowUserPointer(m_Window, this);
+		glfwSetWindowSizeCallback(m_Window, window_resize);
+		glfwSetKeyCallback(m_Window, key_callback);
+		glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
+		glfwSetCursorPosCallback(m_Window, cursor_position_callback);
+
+		if (glewInit() != GLEW_OK)
+		{
+			std::cout << "Failed to initialize GLEW!" << std::endl;
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Window::isKeyPressed(unsigned int keycode) const
+	{
+		// TODO: log this
+		if (keycode >= MAX_KEYS)
+			return false;
+		return m_Keys[keycode];
+	}
+
+	bool Window::isMouseButtonPressed(unsigned int button) const
+	{
+		// TODO: log this
+		if (button >= MAX_BUTTONS)
+			return false;
+		return m_MouseButtons[button];
+	}
+
+	void Window::getMousePosition(double& x, double& y) const
+	{
+		x = mx;
+		y = my;
+	}
+
+	void Window::clear() const
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void Window::update()
+	{
+		glfwPollEvents();
+		glfwGetFramebufferSize(m_Window, &m_Width, &m_Height);
+		glfwSwapBuffers(m_Window);
+	}
+
+	bool Window::closed() const
+	{
+		return glfwWindowShouldClose(m_Window);
+	}
+
+	void window_resize(GLFWwindow* window, int width, int height)
+	{
+		glViewport(0, 0, width, height);
+	}
+
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		Window* win = (Window*)glfwGetWindowUserPointer(window);
+		win->m_Keys[key] = action != GLFW_RELEASE;
+	}
+
+	void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+	{
+		Window* win = (Window*)glfwGetWindowUserPointer(window);
+		win->m_MouseButtons[button] = action != GLFW_RELEASE;
+	}
+
+	void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		Window* win = (Window*)glfwGetWindowUserPointer(window);
+		win->mx = xpos;
+		win->my = ypos;
+	}
+}}
--- RxoGL-core/main.cpp
+++ RxoGL-core/main.cpp
@@ -1,11 +1,11 @@
-#include "window.h"
+#include "graphics/window.h"
 
 int main()
 {
 	using namespace rxogl;
 	using namespace graphics;
 
-	Window window("rxogl", 960, 1040);
+	Window window("rxogl", 960, 540);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
@@ -17,13 +17,27 @@ int main()
 	while (!window.closed())
 	{
 		window.clear();
+		if (window.isKeyPressed(GLFW_KEY_A))
+		{
+			std::cout << "Pressed A" << std::endl;
+		}		
+		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT))
+		{
+			std::cout << "LEFT" << std::endl;
+		}
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x << "," << y << std::endl;
+#if 0
 		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#else
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(-0.5f, 0.5f);
 		glVertex2f( 0.5f, 0.5f);
 		glVertex2f( 0.5f, -0.5f);
 		glEnd();
+#endif
 		window.update();
 	}
 
